59c0a93880a575612d092afab7e04339a7d6d6a7
Karl Vincent Paclar <paclarvincentkarl@gmail.com>
2021-01-19 10:59:53 +0800

    add buttons codes

--- app/src/main/java/mcm/edu/ph/paclar_calculator/MainActivity.java
+++ app/src/main/java/mcm/edu/ph/paclar_calculator/MainActivity.java
@@ -10,5 +10,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btn0 = findViewById(R.id.btn0);
+        btn1 = findViewById(R.id.btn0);
+        btn2 = findViewById(R.id.btn0);
+        btn3 = findViewById(R.id.btn0);
+        btn4 = findViewById(R.id.btn0);
+        btn5 = findViewById(R.id.btn0);
+        btn6 = findViewById(R.id.btn0);
+        btn7 = findViewById(R.id.btn0);
+        btn8 = findViewById(R.id.btn0);
+        btn9 = findViewById(R.id.btn0);
+        btnAdd = findViewById(R.id.btn0);
+        btn0 = findViewById(R.id.btn0);
+
+
+
     }
 }
